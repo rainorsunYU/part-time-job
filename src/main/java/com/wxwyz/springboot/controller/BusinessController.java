f1d95b97e70d0e8fba1bbfb746274f89fc945394
rainorsunYU <487085055@qq.com>
2019-12-16 01:38:27 +0800

    ''

--- src/main/java/com/wxwyz/springboot/controller/BusinessController.java
+++ src/main/java/com/wxwyz/springboot/controller/BusinessController.java
@@ -1,9 +1,17 @@
 package com.wxwyz.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BusinessController {
 
 
+
+    @RequestMapping(value = "/user/info",params = "businessId")
+    public String findUserInfo(){
+
+
+        return "userInfo";
+    }
 }
